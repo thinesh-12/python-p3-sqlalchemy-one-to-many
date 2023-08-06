da158d1753465075b565d7db8af858ccd6938666
= <=>
2023-08-06 10:39:08 -0400

    lab

--- lib/models.py
+++ lib/models.py
@@ -8,3 +8,38 @@ convention = {
 metadata = MetaData(naming_convention=convention)
 
 Base = declarative_base(metadata=metadata)
+
+
+class Game(Base):
+    def __repr__(self):
+        return (
+            f"Game(id={self.id}, "
+            + f"title={self.title}, "
+            + f"platform={self.platform})"
+        )
+
+    __tablename__ = "games"
+
+    id = Column(Integer(), primary_key=True)
+    title = Column(String())
+    genre = Column(String())
+    platform = Column(String())
+    price = Column(Integer())
+
+    reviews = relationship("Review", backref=backref("game"))
+
+
+class Review(Base):
+    def __repr__(self):
+        return (
+            f"Review(id={self.id}, "
+            + f"score={self.score}, "
+            + f"game_id={self.game_id})"
+        )
+
+    __tablename__ = "reviews"
+
+    id = Column(Integer(), primary_key=True)
+    score = Column(Integer())
+    comment = Column(String())
+    game_id = Column(Integer(), ForeignKey("games.id"))
